acbf08f030bad842879c0e440a436e288538bcb5
MikeAVB <mikeavb@yandex.ru>
2026-04-29 13:25:03 +0300

    FEATURE: Реализовать классы BaseHttpHandler, MoviesHandler TEST: Создать базовый тест для пустого массива фильмов

--- src/main/ru/practicum/moviehub/http/MoviesServer.java
+++ src/main/ru/practicum/moviehub/http/MoviesServer.java
@@ -12,6 +12,7 @@ public class MoviesServer {
     public MoviesServer(MoviesStore moviesStore, int port) {
         try {
             server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/movies", new MoviesHandler());
         } catch (IOException e) {
             throw new RuntimeException("Не удалось создать HTTP-сервер", e);
         }
--- src/test/ru/practicum/moviehub/http/MoviesApiTest.java
+++ src/test/ru/practicum/moviehub/http/MoviesApiTest.java
@@ -4,12 +4,29 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.practicum.moviehub.store.MoviesStore;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MoviesApiTest {
+    private static final String BASE = "http://localhost:8080";
+    private static final int PORT = 8080;
+
+    private static MoviesServer server;
+    private static HttpClient client;
 
     @BeforeAll
     static void beforeAll() {
-
+        server = new MoviesServer(new MoviesStore(), PORT);
+        server.start();
+        client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(2)).build();
     }
 
     @BeforeEach
@@ -19,11 +36,29 @@ public class MoviesApiTest {
 
     @AfterAll
     static void afterAll() {
-
+        server.stop();
+        client.close();
     }
 
     @Test
     void getMovies_whenEmpty_returnsEmptyArray() throws Exception {
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create(BASE + "/movies"))
+                .GET()
+                .build();
 
+        HttpResponse<String> resp =
+                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+
+        assertEquals(200, resp.statusCode(), "GET /movies должен вернуть 200");
+
+        String contentTypeHeaderValue =
+                resp.headers().firstValue("Content-Type").orElse("");
+        assertEquals("application/json; charset=UTF-8", contentTypeHeaderValue,
+                "Content-Type должен содержать формат данных и кодировку");
+
+        String body = resp.body().trim();
+        assertTrue(body.startsWith("[") && body.endsWith("]"),
+                "Ожидается JSON-массив");
     }
 }
